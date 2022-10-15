7e59980ce64487486fe324ef2d2c451578a20042
Owen Bonertz <owenbonertz@gmail.com>
2022-10-15 16:31:43 -0400

    2 Legit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,34 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @tasky = Task.new
+  end
+
+  def show
+    @taskito = Task.find(params[:id])
+  end
+
+  def create
+    @cosa = Task.new(task_prams)
+    @cosa.save
+    redirect_to tasks_path(@cosa)
+  end
+
+  def edit
+    @taskerov = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_prams)
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def task_prams
+    params.require(:task).permit(:title, :details)
+  end
+
 end
